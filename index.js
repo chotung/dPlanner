b917d529ee6aacde62d0ba6e6dcaf55153325f5d
Charlie <choelestung@gmail.com>
2020-10-11 14:24:16 -0400

    initialized routes

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const { response } = require('express')
 const express = require('express')
 const helmet = require('helmet')
-require('dotenv')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 const app = express()
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}
+
+mongoose.connect(process.env.DB_URL, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("connected to database")
+});
+
 
 app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.use('/api/user', require('./routes/user'))
+app.use('/api/date', require('./routes/date'))
+
 app.get('/', (req, res) => {
   res.send('homepage')
-})
-
-
-app.get('/date', (req, res) => {
-  res.send('date') 
 })
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`))
