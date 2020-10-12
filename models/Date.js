eae05d55635c5ac7d329f7ce9d1a7eae59e5bce2
Charlie <choelestung@gmail.com>
2020-10-12 13:06:39 -0400

    created post and delete routes

--- models/Date.js
+++ models/Date.js
@@ -2,16 +2,6 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const DateSchema = new Schema({
-  // title: String, // String is shorthand for {type: String}
-  // author: String,
-  // body: String,
-  // comments: [{ body: String, date: Date }],
-  // date: { type: Date, default: Date.now },
-  // hidden: Boolean,
-  // meta: {
-  //   votes: Number,
-  //   favs: Number
-  // },
   name: {
     type: String,
     required: true
--- routes/dates.js
+++ routes/dates.js
@@ -3,10 +3,47 @@ const router = express.Router()
 const Date = require('../models/Date')
 
 // GET A DATE
-router.get('/', (req, res) => {
-  const date = new Date()
-  console.log(date)
-  res.send('date')  
+router.get('/', async (req, res) => {
+  try {
+    const dates = await Date.find({})
+    console.log(dates.length)
+    if(dates.length === 0) {
+      res.json({msg: 'there are no dates'})
+    } else {
+      res.json(dates)
+    }
+  } catch (error) {
+    console.error({ msg: 'no dates'})
+  }
 })
+
+// CREATE A NEW DATE?
+router.post('/', async (req, res) => {
+  const { name, activity, location, time, meta } = req.body
+  try {
+    const newDate = await new Date({
+      name,
+      activity,
+      location,
+      time,
+      meta
+    })
+    newDate.save()
+    res.json({msg: 'Created A New Date'})
+  } catch (error) {
+    console.error({ msg: 'could not create' })
+  }
+})
+// ADMIN PURPOSES
+// DELETE ALL DATES
+router.delete('/delete', async (req, res) => {
+  try {
+    await Date.deleteMany({})
+    res.send('deleted all')
+  } catch (error) {
+    console.error({msg: 'could not delete'})
+  }
+})
+
 
 module.exports = router
